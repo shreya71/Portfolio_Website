2954aaf31b237b92efaa2737fe72df70b9bcf37e
Shreya <shreyasingh070801@gmail.com>
2024-07-06 22:00:51 +0530

    Make corrections

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onscroll = () => {
  ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
  const typed = new Typed('.multiple-text', {
-    strings: ['Software Engineer', 'Developer', 'Fast Learner', 'Gamer `&#128527`'],
+    strings: ['Full-Stack Developer', 'Fast Learner', 'Gamer `&#128527`'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
